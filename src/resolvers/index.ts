761bc52eea2b15fc49a5160f1947185c4ca99d6d
Alexandre Clark <alex.clark.015@gmail.com>
2019-02-03 10:08:51 -0500

    turned off strict mode

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -2,9 +2,8 @@ import Query from "./Queries";
 import Mutation from "./Mutations";
 import Subscription from "./Subscriptions";
 import Nodes from "./Nodes";
-import { MutationResolvers, Resolvers } from "../generated/yoga-client";
 
-const resolvers: Resolvers = {
+const resolvers = {
   Query,
   Subscription,
   Mutation,
