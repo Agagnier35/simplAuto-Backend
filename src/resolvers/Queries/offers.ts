a6004dac682713443263b6977907b4ec30795e55
Alexandre Clark <alex.clark.015@gmail.com>
2019-02-24 13:01:37 -0500

    query an offer by its id

--- src/resolvers/Queries/offers.ts
+++ src/resolvers/Queries/offers.ts
@@ -2,10 +2,15 @@ import { Context } from "../../utils";
 import { QueryResolvers } from "../../generated/yoga-client";
 
 interface OffersQueries {
+  offer: QueryResolvers.OfferResolver;
   offerAddons: QueryResolvers.OfferAddonsResolver;
 }
 
 export const offers: OffersQueries = {
+  offer(parent, { id }, ctx: Context) {
+    return ctx.prisma.offer({ id });
+  },
+
   offerAddons(parent, args, ctx: Context) {
     return ctx.prisma.offerAddons({
       where: {
