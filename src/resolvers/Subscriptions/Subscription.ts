ca75eafe4ff9ab40fd4800bff33c556e684cb9a6
Alexandre Clark <alex.clark.015@gmail.com>
2019-02-24 22:00:40 -0500

    Subscriptions are now working with playground *woot*

--- src/resolvers/Subscriptions/Subscription.ts
+++ src/resolvers/Subscriptions/Subscription.ts
@@ -1,33 +1,29 @@
-import { Context, getUserId } from "../../utils";
+import { Context as Ctx, getUserId } from "../../utils";
+import { Permission } from "../../generated/yoga-client";
+
+interface Context extends Ctx {
+  userId: string;
+  permissions: [Permission];
+}
 
 export const Subscription = {
   messageSubscription: {
     subscribe: async (parent, args, ctx: Context) => {
-      const userID = getUserId(ctx);
+      const userId = ctx.userId;
 
       return ctx.prisma.$subscribe
         .message({
           mutation_in: ["CREATED"],
-          OR: [
-            {
-              node: {
-                conversation: {
-                  buyer: {
-                    id: userID
-                  }
-                }
-              }
-            },
-            {
-              node: {
-                conversation: {
-                  seller: {
-                    id: userID
-                  }
-                }
+          node: {
+            conversation: {
+              buyer: {
+                id: userId
+              },
+              seller: {
+                id: userId
               }
             }
-          ]
+          }
         })
         .node();
     },
