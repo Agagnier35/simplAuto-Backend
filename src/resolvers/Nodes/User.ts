d72ccaf277568ed801be1f1d76945cf0635de02a
Alexandre Clark <alex.clark.015@gmail.com>
2019-03-08 13:15:30 -0500

    Pagination counts for frontend pagination

--- src/resolvers/Nodes/User.ts
+++ src/resolvers/Nodes/User.ts
@@ -1,4 +1,4 @@
-import { Context } from "../../utils";
+import { Context, getUserId } from "../../utils";
 import { UserResolvers } from "../../generated/yoga-client";
 
 export const User: UserResolvers.Type = {
@@ -16,6 +16,21 @@ export const User: UserResolvers.Type = {
     });
   },
 
+  carCount(parent, args, ctx: Context) {
+    const id = getUserId(ctx);
+    return ctx.prisma
+      .carsConnection({
+        where: {
+          status: "PUBLISHED",
+          owner: {
+            id
+          }
+        }
+      })
+      .aggregate()
+      .count();
+  },
+
   ads: ({ id }, { pageSize, pageNumber }, ctx: Context) => {
     const resolverArg: any = {
       where: {
@@ -31,7 +46,44 @@ export const User: UserResolvers.Type = {
     return ctx.prisma.user({ id }).ads(resolverArg);
   },
 
+  adCount(parent, args, ctx: Context) {
+    const id = getUserId(ctx);
+    return ctx.prisma
+      .adsConnection({
+        where: {
+          status: "PUBLISHED",
+          creator: {
+            id
+          }
+        }
+      })
+      .aggregate()
+      .count();
+  },
+
   conversations: ({ id }, args, ctx: Context) => {
     return ctx.prisma.user({ id }).conversations();
+  },
+  conversationCount(parent, args, ctx: Context) {
+    const id = getUserId(ctx);
+    return ctx.prisma
+      .conversationsConnection({
+        where: {
+          OR: [
+            {
+              buyer: {
+                id
+              }
+            },
+            {
+              seller: {
+                id
+              }
+            }
+          ]
+        }
+      })
+      .aggregate()
+      .count();
   }
 };
