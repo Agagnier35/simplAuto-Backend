cda078d9506d52a5446c7d3e440fd5e71f68404a
william chartier <chartier_william@outlook.fr>
2019-03-09 14:58:35 -0500

    offerSuggestion almost done

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,7 @@
+import { Offer } from "../generated/prisma-client";
+
 export interface OfferPosition {
+  offer: Offer | null;
   position: number | null;
   score: number | null;
 }
--- src/resolvers/Queries/offers.ts
+++ src/resolvers/Queries/offers.ts
@@ -1,8 +1,55 @@
 import { Context } from "../../utils";
 import { QueryResolvers } from "../../generated/yoga-client";
-import { Offer, Float } from "../../generated/prisma-client";
+import { Offer, Float, Car, Ad } from "../../generated/prisma-client";
+import { OfferPosition } from "../../models";
 
-function calc_score(weight, car) {}
+function calc_score(offerCar: Car, offer: Offer, ad: Ad) {
+  const weight = {
+    price: 50,
+    manufacturer: 7,
+    model: 7,
+    category: 14,
+    mileage: 14,
+    year: 8
+  };
+
+  const max_deviation = 0.2;
+
+  let total_score = 0;
+
+  //price
+  if (offer.price < ad.priceLowerBound) {
+    const minimum = ad.priceLowerBound * (1 - max_deviation);
+    const gap_ad_minimum = ad.priceLowerBound - minimum;
+    const gap_offer_minimum = offer.price - minimum;
+    const perc_score = gap_offer_minimum / gap_ad_minimum;
+    const weight_score = weight.price * perc_score;
+
+    if (weight_score < 0) {
+      total_score += 0;
+    } else {
+      total_score += weight_score;
+    }
+  } else if (offer.price > ad.priceHigherBound) {
+    const maximum = ad.priceHigherBound * (1 + max_deviation);
+    const gap_ad_maximum = maximum - ad.priceHigherBound;
+    const gap_offer_maximum = maximum - offer.price;
+    const perc_score = gap_offer_maximum / gap_ad_maximum;
+    const weight_score = weight.price * perc_score;
+
+    if (weight_score < 0) {
+      total_score += 0;
+    } else {
+      total_score += weight_score;
+    }
+  } else {
+    total_score += weight.price;
+  }
+
+  // manufacturer
+
+  return;
+}
 
 interface OffersQueries {
   offer: QueryResolvers.OfferResolver;
@@ -24,30 +71,20 @@ export const offers: OffersQueries = {
   },
 
   async suggestions(parent, { id }, ctx: Context) {
-    const weight = {
-      price: 50,
-      manufacturer: 7,
-      model: 7,
-      category: 14,
-      mileage: 14,
-      year: 8
-    };
-
-    type offer_score = {
-      offer: Offer;
-      score: Float;
-      position: number;
-    };
-
-    const max_deviation = 0.4;
-
     const offers = await ctx.prisma.ad({ id }).offers();
     let offers_score = [];
 
     offers.forEach(async element => {
       const offerCar = await ctx.prisma.offer({ id: element.id }).car();
-      let score = calc_score(weight, offerCar);
-      offers_score.push();
+      const score = calc_score(offerCar, element, parent);
+
+      const offer_score: OfferPosition = {
+        offer: element,
+        score: { score },
+        position: null
+      };
+
+      offers_score.push(offer_score);
     });
 
     return offers_score;
