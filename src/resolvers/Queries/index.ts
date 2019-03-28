2101ff823d16082d1d91889ebaf3170396d8ad20
Alexandre Clark <alex.clark.015@gmail.com>
2019-03-27 22:09:05 -0400

    Query users with where param

--- src/resolvers/Queries/index.ts
+++ src/resolvers/Queries/index.ts
@@ -3,6 +3,7 @@ import { ads } from "./ads";
 import { car } from "./car";
 import { offers } from "./offers";
 import { prices } from "./prices";
+import { users } from "./user";
 
 // This is weird but temporary
 // Query will be in seperated folders
@@ -12,5 +13,6 @@ export default {
   ...ads,
   ...car,
   ...offers,
-  ...prices
+  ...prices,
+  ...users
 };
