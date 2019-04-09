f86d4e7f6574160b9f648487c181ebeee61e55cc
Agagnier <felix_agagnier@hotmail.ca>
2019-04-09 01:34:58 -0400

    revert

--- src/resolvers/Mutations/auth.ts
+++ src/resolvers/Mutations/auth.ts
@@ -92,10 +92,8 @@ const createAndInjectToken = (user: User, ctx: Context) => {
   // Set the JWT as a cookie on the response
   ctx.response.cookie("token", token, {
     httpOnly: true,
-    domain: process.env.DOMAIN,
     maxAge: 1000 * 60 * 60 * 24 * 365 // Cookie will last 1 year
   });
-  console.log(ctx.response);
 };
 
 export const auth: AuthResolvers = {
